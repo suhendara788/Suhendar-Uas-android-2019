6db96296fcd83ba81f58b11270f71f4bb8878e56
PeTIK <suhendara788@gmailcom>
2019-05-23 10:30:02 +0700

    dua

--- app/src/main/java/com/example/petik/uas/MainActivity.java
+++ app/src/main/java/com/example/petik/uas/MainActivity.java
@@ -6,10 +6,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private EditText txtinput;
@@ -38,7 +46,42 @@ public class MainActivity extends AppCompatActivity {
         }
 
         private void uraiJSON() {
-            
+            String url ="http://localhost/web%20service/json/Maha.json";
+
+            JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                @Override
+                public void onResponse(JSONArray response) {
+                    try {
+
+
+                        JSONObject transaksi = response.getJSONObject(0);
+                        String id = transaksi.getString("id");
+                        String nama = transaksi.getString("nama");
+                        String asal_daerah = transaksi.getString("asal_daerah");
+                        String kamar = transaksi.getString("kamar");
+
+
+                        tv1.append(id +"\n\n");
+
+
+
+
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            },new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                    Toast.makeText(getApplicationContext(),"Error", Toast.LENGTH_SHORT).show();
+                }
+
+            });
+            mQueue.add(request);
+
         }
     });
 
